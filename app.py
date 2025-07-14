b3ea06e7395b5f87d3d7f75237b8d845512e3f79
kkarthi01 <ksacademysalem>
2025-07-14 17:46:52 +0530

    Initial commit

--- app.py
+++ app.py
@@ -123,3 +123,9 @@ def pgtrb():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
